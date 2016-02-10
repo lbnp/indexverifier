594aea3112fabea4c995f84b750314fa439860b1
Jun Kamoshima <kamoshima@gmail.com>
2016-02-11 00:22:44 +0900

    initial

---
+++ indexverifyer.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <list>
+
+int main()
+{
+    static const uint32_t CACHE_CAPACITY = 14;
+    std::vector<uint32_t> indices;
+    std::list<uint32_t> cache;
+    uint32_t hits = 0;
+
+    for (auto index: indices) {
+        if (std::find(cache.begin(), cache.end(), index) != cache.end()) {
+            hits++;
+        } else {
+            // LRU
+            if (cache.size() == CACHE_CAPACITY) {
+                cache.pop_front();
+                cache.push_back(index);
+            }
+        }
+    }
+
+    float ratio = hits / indices.size() * 100.0f;
+    return 0;
+}
